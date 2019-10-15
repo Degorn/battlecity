c0b1037d58aaf857d032e5faa02d6fdd7e48e9ca
Daniil Ramaniuk <daniil_ramaniuk@epam.com>
2019-10-15 17:59:29 +0300

    Add project files.

---
+++ Battlecity/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using WebSocketSharp;
+using System.Threading.Tasks;
+
+namespace Battlecity
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
